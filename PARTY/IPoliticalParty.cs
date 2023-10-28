0df27cb8ddc6141ea602c4dcc9080367205ed379
kamil osyp <kamilosyp@gmail.com>
2023-10-28 15:28:35 +0200

    Add project files.

---
+++ PARTY/IPoliticalParty.cs
@@ -0,0 +1,17 @@
+﻿
+
+namespace PARTY
+{
+    public interface IPoliticalParty
+    {
+        string Name { get; }
+        void AddScore(float score);
+        void AddScore(int score);
+
+        void AddScore(double score);
+
+        void AddScore(string score);
+
+        Statistics GetStatistics();
+    }
+}
